47a52b98f136d5ae293b7f28f91ab46ec0a16c80
biryaniq <49655956+biryaniq@users.noreply.github.com>
2022-07-06 13:44:17 -0700

    changed ui variable names

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -19,7 +19,7 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        new_task = Contact(content=task_content)
 
         try:
             db.session.add(new_task)
@@ -29,13 +29,13 @@ def index():
             return 'There was an issue adding your task'
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        tasks = Contact.query.order_by(Contact.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = Contact.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
@@ -46,7 +46,7 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    task = Contact.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form['content']
