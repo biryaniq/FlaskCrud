7ce09b690b379ee02244af045b38ec50dc1d4821
biryaniq <49655956+biryaniq@users.noreply.github.com>
2022-07-06 14:35:55 -0700

    fixed update in app.py, now updates all fields

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ db = SQLAlchemy(app)
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(200), nullable=False)
+    number = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -18,15 +20,21 @@ class Contact(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Contact(content=task_content)
+        name = request.form['name']
+        role = request.form['role']
+        number = request.form['number']
+        print(name)
+        # print(role)
+        # print(number)
+        new_task = Contact(name=name, role=role, number=number)
 
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+        except Exception as e:
+            print(e)
+            return 'There was an issue adding your contact'
 
     else:
         tasks = Contact.query.order_by(Contact.date_created).all()
@@ -42,20 +50,22 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deleting that task'
+        return 'There was a problem deleting that contact'
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Contact.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        task.name = request.form['name']
+        task.role = request.form['role']
+        task.number = request.form['number']
 
         try:
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue updating your task'
+            return 'There was an issue updating your contact'
 
     else:
         return render_template('update.html', task=task)
